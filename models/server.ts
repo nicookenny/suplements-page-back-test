22f0a270bd55b00e37bc6df32745690193ab69d5
unknown <celesbmontero@hotmail.com>
2024-07-08 09:14:59 -0300

    creacion de server. Hay que armar las rutas

---
+++ models/server.ts
@@ -0,0 +1,37 @@
+import express, { Express } from "express";
+import cors from "cors";
+
+//import authRoutes from "../routes/auth";
+//import orderRoutes from "../routes/orders";
+
+export class Server {
+  app: Express;
+  port: string | number | undefined;
+  authPath: string;
+  orderPath: string;
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.authPath = "/auth";
+    this.orderPath = "/orders";
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares(): void {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  routes(): void {
+    this.app.use(this.authPath.authRoutes);
+    this.app.use(this.orderPath, orderRoutes);
+  }
+
+  listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Listening at port: ${this.port}`);
+    });
+  }
+}
