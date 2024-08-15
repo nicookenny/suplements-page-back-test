f339c864a1f4a681ef9322a430b96f6cf0daf0dd
unknown <celesbmontero@hotmail.com>
2024-08-15 20:42:49 -0300

    modificacion vercel

--- models/server.ts
+++ models/server.ts
@@ -11,7 +11,7 @@ export class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 8080;
     this.authPath = "/auth";
     this.orderPath = "/orders";
     this.middlewares();
