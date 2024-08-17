6c3214240a21211081ceab85093429c233a0a6b0
unknown <celesbmontero@hotmail.com>
2024-08-17 17:33:39 -0300

    modificacion vercel

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,26 @@
-import { Server } from "../models/server";
+/*import { Server } from "../models/server";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
 dotenv.config();
 const server = new Server();
-server.listen();
+server.listen();*/
+import { Request, Response } from "express";
+import { Server } from "../models/server";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const prisma = new PrismaClient();
+const server = new Server();
+
+export default async (req: Request, res: Response) => {
+  // Define how to handle requests, for example:
+  if (req.method === "GET") {
+    res.status(200).json({ message: "Hello from API!" });
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+};
