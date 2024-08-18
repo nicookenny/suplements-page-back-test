92fb129e0e9e3b3ca10e813e4f6f657e7ad4d93c
unknown <celesbmontero@hotmail.com>
2024-08-17 22:35:56 -0300

    vercel5

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-/*import { Server } from "./models/server";
+import { Server } from "./models/server";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 
@@ -6,21 +6,4 @@ dotenv.config();
 
 export const prisma = new PrismaClient();
 const server = new Server();
-server.listen();*/
-
-import express from "express";
-import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
-import { Server } from "./models/server"; // Asegúrate de que este archivo exponga una clase correcta
-
-dotenv.config();
-
-export const prisma = new PrismaClient();
-
-const app = express();
-const server = new Server(); // Asegúrate de que Server pueda ser instanciado y usado adecuadamente
-
 server.listen();
-
-// Exporta la app para que Vercel la pueda usar
-export default app;
